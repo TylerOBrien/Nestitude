0d661f8451a33fca40a3e32ac0627848f8d200a3
Tyler O'Brien <hello@tylerobrien.com>
2023-09-28 13:40:44 -0400

    Add rudimentary physics checks

--- app/Player/Player1.asm
+++ app/Player/Player1.asm
@@ -4,37 +4,39 @@
 
 .include "../constants.inc"
 
+PLAYER_ON_GROUND = %00000001
+PLAYER_JUMPING   = %00000010
+PLAYER_FALLING   = %00000000
+
+BOX_XMIN = $78
+BOX_XMAX = $a8
+BOX_YMIN = $3f
+BOX_YMAX = $4f
+
+BOX_XMIN_INNER = $79
+BOX_XMAX_INNER = $a7
+BOX_YMIN_INNER = $40
+BOX_YMAX_INNER = $4e
+
 ; ---------------------------------------------------------------
 ; ZeroPage
 ; ---------------------------------------------------------------
 
 .segment "ZEROPAGE"
 
-player_state: .res 1
-player_on_which_platform: .res 1
-player_y_count: .res 1
+player_state:    .res 1
 
-player_x_hi: .res 1
-player_x_lo: .res 1
+player_x_pos_lo: .res 1
+player_x_pos_hi: .res 1
+player_x_vel_lo: .res 1
+player_x_vel_hi: .res 1
 
-player_x_dir: .res 1
-player_x_vel: .res 1
-
-player_y_hi:    .res 1
-player_y_vel:   .res 1
-
-player_ignore_a: .res 1
-
-PLAYER_ON_GROUND = %00000001
-PLAYER_JUMPING   = %00000010
-PLAYER_FALLING   = %00000000
+player_y_pos_lo: .res 1
+player_y_pos_hi: .res 1
+player_y_vel_lo: .res 1
+player_y_vel_hi: .res 1
 
 .importzp controller_state
-.importzp obstruct_id
-.importzp obstruct_x_min
-.importzp obstruct_x_max
-.importzp obstruct_y_min
-.importzp obstruct_y_max
 
 ; ---------------------------------------------------------------
 ; Code
@@ -44,154 +46,224 @@ PLAYER_FALLING   = %00000000
 
 .import buffer_sprite_push_from_a
 
-.proc player_jump
-    ldx player_y_count
-    inx
-    stx player_y_count
-    cpx #4
-    bne jump
-    ldx #0
-    stx player_y_count
+; ------------------
+; player_1_hittest_x
+; ------------------
+.proc player_1_hittest_x
+    lda player_x_vel_hi
+    beq exit
 
-    jump:
-        lda player_y_hi
-        clc
-        sbc player_y_vel
-        sta player_y_hi
+    lda player_x_pos_hi
+    cmp #BOX_XMIN_INNER
+    bcc exit
+    cmp #BOX_XMAX
+    bcs exit
 
-    decrease_vel:
-        ldx player_y_vel
-        dex
-        stx player_y_vel
-        beq stop_jump
+    lda player_y_pos_hi
+    cmp #BOX_YMIN_INNER
+    bcc exit
+    cmp #BOX_YMAX
+    bcs exit
+
+    lda player_x_vel_hi
+    cmp #127
+    bcs hit_right_side
+
+    hit_left_side:
+        lda #BOX_XMIN
+        sta player_x_pos_hi
         jmp exit
 
-    stop_jump:
-        lda #0
-        sta player_state
-        sta player_y_count
-
-    exit:
-        rts
-.endproc
-
-.proc player_1_hittest
-    rts
-.endproc
-
-.proc player_fall
-    ldx player_y_count
-    inx
-    stx player_y_count
-    cpx #4
-    bne fall
-    ldx #0
-    stx player_y_count
-
-    increase_vel:
-        ldx player_y_vel
-        inx
-        stx player_y_vel
-
-    fall:
-        lda player_y_hi
-        clc
-        adc player_y_vel
-        sta player_y_hi
+    hit_right_side:
+        lda #BOX_XMAX
+        sta player_x_pos_hi
 
     exit:
         rts
 .endproc
 
 ; ------------------
-; player_1_handle_dpad_left
+; player_1_hittest_y
 ; ------------------
-.proc player_1_handle_dpad_left
-    ldx player_x_hi
-    dex
-    dex
-    stx player_x_hi
-    rts
+.proc player_1_hittest_y
+    lda player_y_vel_hi
+    beq exit
+
+    lda player_x_pos_hi
+    cmp #BOX_XMIN_INNER
+    bcc exit
+    cmp #BOX_XMAX
+    bcs exit
+
+    lda player_y_pos_hi
+    cmp #BOX_YMIN_INNER
+    bcc exit
+    cmp #BOX_YMAX
+    bcs exit
+
+    lda player_y_vel_hi
+    cmp #127
+    bcs hit_bottom_side
+
+    hit_top_side:
+        lda #BOX_YMIN
+        sta player_y_pos_hi
+        jmp exit
+
+    hit_bottom_side:
+        lda #BOX_YMAX
+        sta player_y_pos_hi
+
+    exit:
+        rts
 .endproc
 
 ; ------------------
-; player_1_handle_dpad_right
+; player_1_check_controller_x
 ; ------------------
-.proc player_1_handle_dpad_right
-    ldx player_x_hi
-    inx
-    inx
-    stx player_x_hi
-    rts
-.endproc
-
-; ------------------
-; player_1_check_controller
-; ------------------
-.proc player_1_check_controller
-    lda controller_state
-    and #DPAD_UP
-    beq check_down
-
-    ldy player_y_hi
-    dey
-    sty player_y_hi
-
-    check_down:
+.proc player_1_check_controller_x
+    check_left:
         lda controller_state
-        and #DPAD_DOWN
-        beq check_state
-
-        ldy player_y_hi
-        iny
-        sty player_y_hi
-
-    check_state:
-        lda player_state
-        and #PLAYER_ON_GROUND
+        and #DPAD_LEFT
         beq check_right
-
-    check_a:
-        lda controller_state
-        and #BUTTON_A
-        beq check_if_on_ground
-        lda player_ignore_a
-        bne check_right
-
-    handle_a:
-        lda player_state
-        eor #PLAYER_ON_GROUND
-        ora #PLAYER_JUMPING
-        sta player_state
-        lda #10
-        sta player_ignore_a
-        sta player_y_vel
-        jmp check_right
-
-    check_if_on_ground:
-        lda player_state
-        and #PLAYER_ON_GROUND
-        beq check_right
-        lda #0
-        sta player_ignore_a
+        ldx player_x_vel_hi
+        dex
+        stx player_x_vel_hi
+        jmp exit
 
     check_right:
         lda controller_state
         and #DPAD_RIGHT
-        beq check_left
-
-    handle_right:
-        jsr player_1_handle_dpad_right
+        beq no_press
+        ldx player_x_vel_hi
+        inx
+        stx player_x_vel_hi
         jmp exit
 
-    check_left:
+    no_press:
+        lda #0
+        sta player_x_vel_hi
+
+    exit:
+        rts
+.endproc
+
+; ------------------
+; player_1_check_controller_y
+; ------------------
+.proc player_1_check_controller_y
+    check_up:
         lda controller_state
-        and #DPAD_LEFT
-        beq exit
-
-    handle_left:
-        jsr player_1_handle_dpad_left
+        and #DPAD_UP
+        beq check_down
+        ldx player_y_vel_hi
+        dex
+        stx player_y_vel_hi
         jmp exit
+
+    check_down:
+        lda controller_state
+        and #DPAD_DOWN
+        beq no_press
+        ldx player_y_vel_hi
+        inx
+        stx player_y_vel_hi
+        jmp exit
+
+    no_press:
+        lda #0
+        sta player_y_vel_hi
+
+    exit:
+        rts
+.endproc
+
+; ------------------
+; player_draw
+; ------------------
+.proc player_draw
+    lda player_y_pos_hi
+    jsr buffer_sprite_push_from_a
+    lda #1
+    jsr buffer_sprite_push_from_a
+    lda #2
+    jsr buffer_sprite_push_from_a
+    lda player_x_pos_hi
+    jsr buffer_sprite_push_from_a
+    rts
+.endproc
+
+; ------------------
+; player_1_apply_velocity_x
+; ------------------
+.proc player_1_apply_velocity_x
+    lda player_x_vel_hi
+    cmp #0
+    beq exit
+    cmp #127
+    bcs check_negative
+
+    check_positive:
+        cmp #1
+        beq apply
+
+    prevent_exceeding_max:
+        lda #1
+        sta player_x_vel_hi
+
+    apply:
+        lda player_x_pos_hi
+        clc
+        adc player_x_vel_hi
+        sta player_x_pos_hi
+        jmp exit
+
+    check_negative:
+        cmp #255
+        beq apply
+
+    prevent_exceeding_min:
+        lda #255
+        sta player_x_vel_hi
+        jmp apply
+
+    exit:
+        rts
+.endproc
+
+; ------------------
+; player_1_apply_velocity_y
+; ------------------
+.proc player_1_apply_velocity_y
+    lda player_y_vel_hi
+    cmp #0
+    beq exit
+    cmp #127
+    bcs check_negative
+
+    check_positive:
+        cmp #1
+        beq apply
+
+    prevent_exceeding_max:
+        lda #1
+        sta player_y_vel_hi
+
+    apply:
+        lda player_y_pos_hi
+        clc
+        adc player_y_vel_hi
+        sta player_y_pos_hi
+        jmp exit
+
+    check_negative:
+        cmp #255
+        beq apply
+
+    prevent_exceeding_min:
+        lda #255
+        sta player_y_vel_hi
+        jmp apply
 
     exit:
         rts
@@ -202,36 +274,13 @@ PLAYER_FALLING   = %00000000
 ; ------------------
 .export player_update
 .proc player_update
-    lda player_state
-    and #PLAYER_ON_GROUND
-    beq in_air
-    jmp check_controller
-
-    in_air:
-        lda player_state
-        and #PLAYER_JUMPING
-        beq fall
-        jsr player_jump
-        jmp check_controller
-
-    fall:
-        jsr player_fall
-        jmp check_controller
-
-    check_controller:
-        jsr player_1_check_controller
-
-    jsr player_1_hittest
-
-    lda player_y_hi
-    jsr buffer_sprite_push_from_a
-    lda #1
-    jsr buffer_sprite_push_from_a
-    lda #2
-    jsr buffer_sprite_push_from_a
-    lda player_x_hi
-    jsr buffer_sprite_push_from_a
-
+    jsr player_1_check_controller_x
+    jsr player_1_apply_velocity_x
+    jsr player_1_hittest_x
+    jsr player_1_check_controller_y
+    jsr player_1_apply_velocity_y
+    jsr player_1_hittest_y
+    jsr player_draw
     rts
 .endproc
 
@@ -240,17 +289,20 @@ PLAYER_FALLING   = %00000000
 ; ------------------
 .export player_init
 .proc player_init
-    lda #132
-    sta player_x_hi
-    sta player_y_hi
     lda #0
-    sta player_on_which_platform
-    sta player_ignore_a
-    sta player_x_lo
-    sta player_x_dir
-    sta player_x_vel
-    sta player_y_vel
-    sta player_y_count
+    sta player_x_pos_lo
+    sta player_y_pos_lo
+    sta player_x_vel_lo
+    sta player_y_vel_lo
+    sta player_x_vel_hi
+    sta player_y_vel_hi
+
+    lda #132
+    sta player_x_pos_hi
+    sta player_y_pos_hi
+
+    lda #PLAYER_ON_GROUND
     sta player_state
+
     rts
 .endproc
