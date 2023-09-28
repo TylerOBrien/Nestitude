77936b3b3922084df9061f4da1d78d32d3c8d021
Tyler O'Brien <hello@tylerobrien.com>
2023-09-28 11:19:39 -0400

    Add clock timing logic

--- app/Chrono/Clock.asm
+++ app/Chrono/Clock.asm
@@ -22,10 +22,35 @@ clock_min:   .res 1
 .proc clock_tick
     lda clock_ms_lo
     clc
-    adc #16
-    lda #clock_ms_hi
+    adc #83
+    sta clock_ms_lo
+
+    lda clock_ms_hi
     adc #0
-    rts
+    sta clock_ms_hi
+
+    cmp #20
+    bne exit
+
+    lda clock_sec
+    clc
+    adc #1
+    sta clock_sec
+    cmp #60
+    bne reset_ms_hi
+    lda clock_min
+    clc
+    adc #1
+    sta clock_min
+    lda #0
+    sta clock_sec
+
+    reset_ms_hi:
+        lda #0
+        sta clock_ms_hi
+
+    exit:
+        rts
 .endproc
 
 ; ------------------
