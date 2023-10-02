cc79ed724ad58cc8b3a10bfc55ae72780d166ac9
Tyler O'Brien <hello@tylerobrien.com>
2023-10-01 22:18:02 -0400

    Kepe count of frames modulo'd to 60

--- app/Chrono/Tick.asm
+++ app/Chrono/Tick.asm
@@ -10,8 +10,9 @@
 
 .segment "ZEROPAGE"
 
-tick_count_lo: .res 1
-tick_count_hi: .res 1
+tick_count_lo:    .res 1
+tick_count_hi:    .res 1
+frame_sec_modulo: .res 1
 
 .exportzp tick_count_lo
 .exportzp tick_count_hi
@@ -27,14 +28,30 @@ tick_count_hi: .res 1
 ; ------------------
 .export tick_update
 .proc tick_update
-    lda tick_count_lo
-    clc
-    adc #1
-    sta tick_count_lo
-    lda tick_count_hi
-    adc #0
-    sta tick_count_hi
-    rts
+    check_frame_sec:
+        lda frame_sec_modulo
+        cmp #60
+        beq increment_frame_sec
+        lda #0
+        sta frame_sec_modulo
+        jmp increment_tick_count
+
+    increment_frame_sec:
+        clc
+        adc #1
+        sta frame_sec_modulo
+
+    increment_tick_count:
+        lda tick_count_lo
+        clc
+        adc #1
+        sta tick_count_lo
+        lda tick_count_hi
+        adc #0
+        sta tick_count_hi
+
+    exit:
+        rts
 .endproc
 
 ; ------------------
